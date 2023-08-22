bf12aeb3e8ca213778aa026e7a6fcfb901060654
hariom patidar <hariompatidar32498.com>
2023-08-22 23:32:21 +0530

    perform crud operation on rails controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,44 @@
 class PostsController < ApplicationController
   skip_before_action :verify_authenticity_token
-  def index
-    if params[:status]=="activated"
-      render json: {message: "activated"}
-    else
-      render json: {message: "not activated"}
-    end
-  end 
 
+ 
   def create
     @post = Post.new(set_params)
     if @post.save
-      render json: { message:"User Created", data: @post}
+      render json: @post,status: :created
     else
       render json: { errors: @post.errors.full_messages }
     end
   end
 
-  def set_params
-    params.permit(:name,:age,:email)
+  def index
+    @post=Post.all
+    render json: @post
+  end 
+
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
   end
+
+  def update 
+    @post = Post.find(params[:id])
+    if @post.update(set_params)
+      render json: @post
+    else 
+      render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity
+    end 
+  end 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    render json: { message: "Post successfully deleted" }
+  end
+
+  private
+  def set_params
+    params.permit(:name,:age,:address)
+  end
+
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-#  get "/posts/:status", to:  "posts#index"  #rounting parameter
- get "/posts/", to:  "posts#index"
-#  get "/urlposts", to:  "posts#create"
+ # Custom route for listing all posts
+ get "/posts", to: "posts#index"
+
+ # Custom route for creating a post
  post "/urlposts", to: "posts#create"
+
+ # Custom route for showing a specific post
+ get "/posts/show/:id", to: "posts#show"
+
+ # Custom route for updating a specific post
+ put "/posts/update/:id", to: "posts#update"
+ patch "/posts/update/:id", to: "posts#update" # You can also use PATCH
+
+ # Custom route for deleting a specific post
+ delete "/posts/delete/:id", to: "posts#destroy"
 end
